c7479910398128906425beee9c72f37d9ba3f8df
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2024-03-24 20:08:16 -0400

    Filtered out any posts that don't include a characters name in the title, or aren't posts about things like "Who wins", or "Tier List"

--- main.py
+++ main.py
@@ -2,11 +2,23 @@ import os
 import praw
 from fuzzywuzzy import fuzz
 import pandas as pd
+import csv
 
 '''
 PLAN:
 - Get all of the data/comments, from each post (only posts labeled analysis)
+- Combine the upvotes, downvotes, and Sentiment Analysis score into a single popularity score for each character.
 - 
+- Clean text: (WATCH VIDEO)
+- Sentiment analyzer class
+    - Inputs: Cleaned_text, characters within text. 
+    - Outputs: Dictionary of character --> sentiment score
+- Final Bounty calculator (function or class TBD)
+    - Inputs: For each character...
+        - total upvotes
+        - total downvotes
+        - average sentiment score among all comments regarding them
+    - Outputs: Bounty Value
 '''
 
 # When using an API, you don't want to give out sensitive information in your code directly.
@@ -29,14 +41,28 @@ time_range = 'week'  # can also use day, month, year
 
 #Creates a list of the top posts (number of posts = limit) from our time_range (in this case a week)
 post_list = subreddit.top(time_filter = time_range,limit=25)
-print(type(post_list))
+
+
+inclusion_phrases = ['Who wins', "Tier List"]
+character_list = []
+with open('characters.csv',newline='') as file:
+    csv_reader = csv.reader(file)
+    for row in csv_reader:
+        character_list.extend([value for value in row if value])
+
+
+
+def is_valid_post(post_title):
+    for word in post_title.split():
+        if word in character_list or word in inclusion_phrases:
+            return True
+    return False
 
 filtered_posts = []
 for post in post_list:
-    if post.link_flair_text != "Poll":
+    if is_valid_post(post.title):
+        print(post.title)
 
 
 
-
-
-
+#TODO: Once, posts are filtered, now filter each comment using a similar criteria.
